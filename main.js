b3be5a7cfc10d1f6a540905e97116833fb8a4ada
Den107 <deniskrv@gmail.com>
2021-03-03 21:28:38 +0300

    all done exept burger

--- main.js
+++ main.js
@@ -1,4 +1,15 @@
 let slider = tns({
-  "container": '.my-slider',
-  "items": 3,
+  container: '.my-slider',
+  items: 3,
+  responsive: {
+    318: {
+      items: 1
+    },
+    612: {
+      items: 2
+    },
+    1200: {
+      items: 3
+    }
+  }
 });
